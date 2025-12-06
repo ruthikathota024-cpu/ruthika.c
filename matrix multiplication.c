3ca8d2cb12b80f4ed97e1276b38d13c98c9b9b0a
ruthikathota024-cpu <ruthikathota024@gmail.com>
2025-12-06 22:38:35 +0530

    Create matrix multiplication.c

---
+++ matrix multiplication.c
@@ -0,0 +1,42 @@
+#include<stdio.h>
+main()
+{
+	int a[2][2],b[2][2],c[2][2];
+	int i,j,k;
+		printf("enter 4 elements of matrix a :");
+	for(i=0;i<2;i++)
+	{
+		for(j=0;j<2;j++)
+		{
+			scanf("%d",&a[i][j]);
+		} 
+	}
+	printf("enter 4 elements of matrix b :");
+	for(i=0;i<2;i++)
+	{
+		for(j=0;j<2;j++)
+		{
+			scanf("%d",&b[i][j]);
+		} 
+	}
+
+
+	for(i=0;i<2;i++)
+	{
+		for(j=0;j<2;j++)
+		{
+			c[i][j]=0;
+			for(k=0;k<2;k++)
+			{c[i][j]=c[i][j]+a[i][j]*b[i][j];
+			}
+		}
+	 }
+		printf("the matrix multiplication :\n");
+		for(i=0;i<2;i++)
+		{
+			for(j=0;j<2;j++)
+			{
+				printf("%d\t",c[i][j]);
+			} printf("\n");
+		}
+}

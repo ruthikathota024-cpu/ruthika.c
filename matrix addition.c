7b88cbacc9e1dbf39c3deb3fe322bd5aeebd37f2
ruthikathota024-cpu <ruthikathota024@gmail.com>
2025-12-06 22:29:40 +0530

    Create matrix addition.c

---
+++ matrix addition.c
@@ -0,0 +1,41 @@
+#include<stdio.h>
+main()
+{
+	int a[2][2],b[2][2],c[2][2];
+	int i,j;
+	printf("enter 4 elements of matrix a :");
+	for(i=0;i<2;i++)
+	{
+		for(j=0;j<2;j++)
+		{
+			scanf("%d",&a[i][j]);
+		}
+	}
+	printf("enter 4 elements of matrix b :");
+	for(i=0;i<2;i++)
+	{
+		for(j=0;j<2;j++)
+		{
+			scanf("%d",&b[i][j]);
+		}
+	}
+
+	for(i=0;i<2;i++)
+	{
+		for(j=0;j<2;j++)
+		{ c[i][j]=a[i][j]+b[i][j];
+			
+		}
+	}
+		printf("the matrix addition is :\n");
+		for(i=0;i<2;i++)
+		{
+			for(j=0;j<2;j++)
+			{
+				printf("%d\t",c[i][j]);
+			}
+			printf("\n");
+		}
+	
+	
+}
